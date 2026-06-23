2db1a9b811b2bb3ebd8e946b1655ebc637975565
dilipramesh3082006-droid <dilipramesh3082006@gmail.com>
2026-06-23 10:42:55 +0530

    Delete numpi.py

--- numpi.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-marks = np.array([75, 80, 90, 85, 95])
-
-print("Marks:", marks)
-print("Highest:", np.max(marks))
-print("Lowest:", np.min(marks))
-print("Average:", np.mean(marks))
